5284370a10f042d01f57bd375fe86658ea472980
sagarsaji1999 <133841251+sagarsaji1999@users.noreply.github.com>
2023-05-26 11:24:04 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,17 @@
+
+ function increment()
+{
+    let s= document.getElementById("a")
+    s.innerHTML=Number(s.innerHTML)+1;
+}
+function decrement()
+{
+    let s=document.getElementById("a")
+    s.innerHTML=Number(s.innerHTML)-1;
+}
+function reset()
+{
+    let s=document.getElementById("a")
+    s.innerHTML=0;
+}
+
